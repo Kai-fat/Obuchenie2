06b6af98645df5e1119a7e82cdec5f2a523805c2
Khabukin <dihab@mail.ru>
2022-11-24 00:07:28 +0300

    Example004 Div Double

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double namberA =14;
+double namberB = 6;
+Console.WriteLine(namberA / namberB);
