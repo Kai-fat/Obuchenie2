069aa72d2ce2311e97be1e4651e6c007945184c4
Khabukin <dihab@mail.ru>
2022-11-26 22:18:39 +0300

    Example005_ if Tlse

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Обзовись:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "вася")
+{
+    Console.WriteLine("Ну здравстуй, Вася");
+}
+else
+{
+    Console.Write("Привет, чувак.");
+    Console.WriteLine(username);
+}
+
